dfbf5caf66ee2e7853e9c9b27e6eb817eb636d96
brianmsj <brianmsj@gmail.com>
2017-04-17 08:57:47 -0700

    added button link and styling

--- client/src/components/login-page.js
+++ client/src/components/login-page.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
 export default function LoginPage() {
-    return <a href={'/api/auth/google'}>Login with Google</a>;
+    return(
+       <button className="loginBtn loginBtn--google">
+        <a href={'/api/auth/google'}>Login with Google</a>
+       </button>
 }
