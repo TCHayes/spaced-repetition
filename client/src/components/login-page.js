40fd33a8f2c74713be806032b1022fea5e1ca0fb
brianmsj <brianmsj@gmail.com>
2017-04-17 10:57:57 -0700

    updated font

--- client/src/components/login-page.js
+++ client/src/components/login-page.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function LoginPage() {
     return(
       <div className='login-container'>
-       <h1 className='main-title'>Periodic Repitition</h1>
-       <h3 className="slogan">A Simple Way to Learn the Periodic Table using Spaced Repitition</h3>
+       <h1 className='main-title'>Periodic Repetition</h1>
+       <h3 className="slogan">A Simple Way to Learn the Periodic Table using Spaced Repetition</h3>
        <button className="loginBtn loginBtn--google">
         <a href={'/api/auth/google'}>Login with Google</a>
        </button>
