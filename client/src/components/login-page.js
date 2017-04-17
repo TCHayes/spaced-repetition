552c7f4cdfd10d329b0b6437d87a0d5b06eb5fbe
brianmsj <brianmsj@gmail.com>
2017-04-17 09:31:33 -0700

    changed CSS and added functionality to login

--- client/src/components/login-page.js
+++ client/src/components/login-page.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 export default function LoginPage() {
     return(
+      <div className='login-container'>
+       <h1 className='main-title'>Periodic Repitition</h1>
+       <h3 className="slogan">A Simple Way to Learn the Periodic Table using Spaced Repitition</h3>
        <button className="loginBtn loginBtn--google">
         <a href={'/api/auth/google'}>Login with Google</a>
        </button>
+      </div>
+    )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import './index.css';
+import './login.css';
 
 ReactDOM.render(
   <App />,
