c492767f64b6e53fae81155ba7d1fa8fa98c14f5
Terry Hayes <terrychayes@gmail.com>
2017-04-18 09:43:35 -0400

    import Cookies, QuestionPage & LoginPage, and react-router; add checkAuth function and routes for App, QuestionPage, and LoginPage

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import * as Cookies from 'js-cookie';
+import QuestionPage from './components/question-page';
+import LoginPage from './components/login-page';
 import './index.css';
 import './login.css';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+// import {createStore, applyMiddleware} from 'redux';
+// import thunk from 'redux-thunk';
+// import {Provider} from 'react-redux';
+
+function checkAuth() {
+  const accessToken = Cookies.get('accessToken');
+  if (!accessToken){
+    browserHistory.replace('/login');
+  }
+}
+
+const routes = (
+  <Router history={browserHistory}>
+    <Route path='/' component={App}>
+      <IndexRoute component={QuestionPage} onEnter={checkAuth} />
+      <Route path='/login' component={LoginPage} />
+    </Route>
+  </Router>
+);
 
 ReactDOM.render(
-  <App />,
+  <div>
+    {routes}
+  </div>,
   document.getElementById('root')
 );
