1fbc4b09c7ea9e5f6aab8f6ad82204edad44c82a
Terry Hayes <terrychayes@gmail.com>
2017-04-18 17:03:15 -0400

    update reducer to handle fetchQuestion action

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,7 @@
 import * as actions from '../actions/index';
 
 const initialState = {
-  questions: [],
+  question: '',
   name: '',
   picture: '',
   score: 0,
@@ -10,13 +10,19 @@ const initialState = {
 
 export default (state=initialState, action) => {
   if (action.type === actions.FETCH_USER_SUCCESS){
-    return {...state, questions: action.questions,
+    return {...state,
       name: action.name,
       picture: action.picture,
       error: null,
     }
   }
   if (action.type === actions.FETCH_USER_FAILURE){
+    return {...state, error: action.error}
+  }
+  if (action.type === actions.FETCH_QUESTION_SUCCESS){
+    return {...state, question: action.question}
+  }
+  if (action.type === actions.FETCH_QUESTION_FAILURE){
     return {...state, error: action.error}
   }
   return state;
