f1783bc014aa6ee1f3f61f4d12dba77b8a29897f
Terry Hayes <terrychayes@gmail.com>
2017-04-18 15:08:27 -0400

    remove unused imports

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import * as Cookies from 'js-cookie';
-import { browserHistory } from 'react-router';
 import * as actions from '../actions';
 import { connect } from 'react-redux';
 
