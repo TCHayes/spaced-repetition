3bfc7cc5211bbb8ace1c0b67af2c276ca000123e
Terry Hayes <terrychayes@gmail.com>
2017-04-18 15:03:43 -0400

    move fetch from componentDidMount to redux actions.

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,43 +1,13 @@
 import React from 'react';
 import * as Cookies from 'js-cookie';
 import { browserHistory } from 'react-router';
-// import QuestionPage from './question-page';
-// import LoginPage from './login-page';
+import * as actions from '../actions';
+import { connect } from 'react-redux';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentUser: null
-        };
-    }
 
     componentDidMount() {
-        // Job 4: Redux-ify all of the state and fetch calls to async actions.
-        const accessToken = Cookies.get('accessToken');
-        if (accessToken) {
-            fetch('/api/me', {
-                headers: {
-                    'Authorization': `Bearer ${accessToken}`
-                }
-            }).then(res => {
-                if (!res.ok) {
-                    if (res.status === 401) {
-                        // Unauthorized, clear the cookie and go to
-                        // the login page
-                        Cookies.remove('accessToken');
-                        browserHistory.replace('/login');
-                        return;
-                    }
-                    throw new Error(res.statusText);
-                }
-                return res.json();
-            }).then(currentUser =>
-                this.setState({
-                    currentUser
-                })
-            );
-        }
+        this.props.dispatch(actions.fetchUser());
     }
 
     render() {
@@ -49,4 +19,4 @@ class App extends React.Component {
     }
 }
 
-export default App;
+export default connect()(App);
